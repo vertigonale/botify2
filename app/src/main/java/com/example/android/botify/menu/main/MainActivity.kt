7dbbfc77c9b1c52bfab31f9e5ad9b2e9c4e7120e
Julia Zuzana Djanic <julia.djanic@stud.fh-campuswien.ac.at>
2022-01-11 17:44:05 +0100

    transfer mediaBrowser functionality from audiofragment to mainactivity

--- app/src/main/java/com/example/android/botify/menu/main/MainActivity.kt
+++ app/src/main/java/com/example/android/botify/menu/main/MainActivity.kt
@@ -1,17 +1,19 @@
 package com.example.android.botify.menu.main
 
+import android.app.Activity
+import android.content.ComponentName
 import android.os.Bundle
+import android.support.v4.media.MediaBrowserCompat
+import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
-import android.view.MenuItem
-import androidx.core.view.get
 import androidx.databinding.DataBindingUtil
 import com.example.android.botify.R
 import com.example.android.botify.databinding.ActivityMainBinding
-import com.example.android.botify.menu.sub.audio.AudioFragment
+import com.example.android.botify.menu.sub.audio.AudioService
 
 class MainActivity : AppCompatActivity() {
     private val LOG_TAG = this::class.java.simpleName
@@ -19,10 +21,61 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 //    private lateinit var menuTitle: String
 
-    companion object {
-        private var audioFragment: AudioFragment
-        init {
-            audioFragment = AudioFragment()
+    private lateinit var mediaBrowser: MediaBrowserCompat
+
+    private val connectionCallbacks = object : MediaBrowserCompat.ConnectionCallback() {
+
+        override fun onConnected() {
+            val methodName = object{}.javaClass.enclosingMethod?.name
+            Log.i(LOG_TAG, methodName!!)
+
+            mediaBrowser.sessionToken.also { token ->
+                val audioController = MediaControllerCompat(
+                    this@MainActivity,
+                    token
+                )
+
+                MediaControllerCompat.setMediaController(this@MainActivity, audioController)
+            }
+
+            buildTransportProtocols()
+        }
+
+        override fun onConnectionSuspended() {
+            val methodName = object{}.javaClass.enclosingMethod?.name
+            Log.i(LOG_TAG, methodName!!)
+//            super.onConnectionSuspended()
+        }
+
+        override fun onConnectionFailed() {
+            val methodName = object{}.javaClass.enclosingMethod?.name
+            Log.i(LOG_TAG, methodName!!)
+//            super.onConnectionFailed()
+        }
+    }
+
+    private var controllerCallbacks = object : MediaControllerCompat.Callback() {
+
+        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
+            val methodName = object{}.javaClass.enclosingMethod?.name
+            Log.i(LOG_TAG, methodName!!)
+
+            super.onMetadataChanged(metadata)
+        }
+
+        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
+            val methodName = object{}.javaClass.enclosingMethod?.name
+            Log.i(LOG_TAG, methodName!!)
+
+            super.onPlaybackStateChanged(state)
+        }
+
+        override fun onSessionDestroyed() {
+            val methodName = object{}.javaClass.enclosingMethod?.name
+            Log.i(LOG_TAG, methodName!!)
+
+            mediaBrowser.disconnect()
+            // maybe schedule a reconnection using a new MediaBrowser instance
         }
     }
 
@@ -31,13 +84,20 @@ class MainActivity : AppCompatActivity() {
         Log.i(LOG_TAG, methodName!!)
 
         super.onCreate(savedInstanceState)
+
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setSupportActionBar(binding.toolbar)
 
 //        binding.bottomAppBar.menu[R.id.menu_name].title.
 
+        mediaBrowser = MediaBrowserCompat(
+            this,
+            ComponentName(this, AudioService::class.java),
+            connectionCallbacks,
+            null //optional Bundle
+        )
 
-
+        Log.i(LOG_TAG, "$methodName + mediaBrowser")
     }
 
     //TODO check if necessary (app runs without crashes when commented out)
@@ -52,26 +112,24 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.bottom_app_bar, menu)
         Log.i(LOG_TAG, "$methodName + bottom_app_bar?")
 
-        binding.bottomAppBar.setOnMenuItemClickListener { menuItem ->
+/*        binding.bottomAppBar.setOnMenuItemClickListener { menuItem ->
             when (menuItem.itemId) {
                 R.id.play_pause -> {
 
                     // TODO play pause
-                    val mediaController = audioFragment.getMediaController()
+                    val mediaController = getMainMediaController()
 
-                    if (mediaController != null) {
-                        val pbStateTest = mediaController.playbackState?.state
-                        Log.i(LOG_TAG, "$methodName mC pS?: " + pbStateTest.toString())
+                    val pbStateTest = mediaController.playbackState.state
+                    Log.i(LOG_TAG, "$methodName mC pS?: " + pbStateTest.toString())
 
-                        if (pbStateTest == PlaybackStateCompat.STATE_PLAYING) {
-                            Log.i(LOG_TAG, "$methodName + sOCL if 1: " + pbStateTest.toString())
-                            mediaController.transportControls.pause()
-                            Log.i(LOG_TAG, "$methodName + sOCL if 2: " + pbStateTest.toString())
-                        } else {
-                            Log.i(LOG_TAG, "$methodName + sOCL else 1: " + pbStateTest.toString())
-                            mediaController.transportControls.play()
-                            Log.i(LOG_TAG, "$methodName + sOCL else 2: " + pbStateTest.toString())
-                        }
+                    if (pbStateTest == PlaybackStateCompat.STATE_PLAYING) {
+                        Log.i(LOG_TAG, "$methodName + sOCL if 1: " + pbStateTest.toString())
+                        mediaController.transportControls.pause()
+                        Log.i(LOG_TAG, "$methodName + sOCL if 2: " + pbStateTest.toString())
+                    } else {
+                        Log.i(LOG_TAG, "$methodName + sOCL else 1: " + pbStateTest.toString())
+                        mediaController.transportControls.play()
+                        Log.i(LOG_TAG, "$methodName + sOCL else 2: " + pbStateTest.toString())
                     }
                     // TODO maybe a toast telling there is no audio yet selected
 //                    else {}
@@ -84,7 +142,7 @@ class MainActivity : AppCompatActivity() {
                 }
                 else -> false
             }
-        }
+        }*/
 
 /*        binding.bottomAppBar.setOnMenuItemClickListener { menuI
         }*/
@@ -101,6 +159,76 @@ class MainActivity : AppCompatActivity() {
 //            else -> super.onOptionsItemSelected(item)
 //        }
 //    }
+
+    private fun buildTransportProtocols() {
+        val methodName = object{}.javaClass.enclosingMethod?.name
+        Log.i(LOG_TAG, methodName!!)
+
+        val mediaController = getMainMediaController()
+
+        binding.bottomAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.play_pause -> {
+
+                    val pbStateTest = mediaController.playbackState.state
+                    Log.i(LOG_TAG, "$methodName mC pS?: " + pbStateTest.toString())
+
+                    if (pbStateTest == PlaybackStateCompat.STATE_PLAYING) {
+                        Log.i(LOG_TAG, "$methodName + sOCL if 1: " + pbStateTest.toString())
+                        mediaController.transportControls.pause()
+                        Log.i(LOG_TAG, "$methodName + sOCL if 2: " + pbStateTest.toString())
+                    } else {
+                        Log.i(LOG_TAG, "$methodName + sOCL else 1: " + pbStateTest.toString())
+                        mediaController.transportControls.play()
+                        Log.i(LOG_TAG, "$methodName + sOCL else 2: " + pbStateTest.toString())
+                    }
+                    // TODO maybe a toast telling there is no audio yet selected
+//                    else {}
+
+                    true
+                }
+                R.id.more -> {
+                    // TODO more
+                    true
+                }
+                else -> false
+            }
+        }
+
+        // Display the initial state
+        val metadata = mediaController.metadata
+        val pbState = mediaController.playbackState
+
+        // Register a Callback to stay in sync
+        mediaController.registerCallback(controllerCallbacks)
+    }
+
+    fun getMainMediaController() : MediaControllerCompat {
+
+        return MediaControllerCompat.getMediaController(this)
+    }
+
+    override fun onStart() {
+        val methodName = object{}.javaClass.enclosingMethod?.name
+        Log.i(LOG_TAG, methodName!!)
+
+        super.onStart()
+        if (!mediaBrowser.isConnected) {
+            Log.i(LOG_TAG, "$methodName mB con? " + mediaBrowser.isConnected)
+
+            mediaBrowser.connect()
+            Log.i(LOG_TAG, "$methodName mB con? " + mediaBrowser.isConnected)
+        }
+/*        val mediaController = getMainMediaController()
+        Log.i(LOG_TAG, "$methodName + $mediaController")*/
+    }
+
+    override fun onStop() {
+
+        getMainMediaController().unregisterCallback(controllerCallbacks)
+
+        super.onStop()
+    }
 
     override fun onDestroy() {
         val methodName = object{}.javaClass.enclosingMethod?.name
--- app/src/main/java/com/example/android/botify/menu/sub/audio/AudioFragment.kt
+++ app/src/main/java/com/example/android/botify/menu/sub/audio/AudioFragment.kt
@@ -1,12 +1,9 @@
 package com.example.android.botify.menu.sub.audio
 
-import android.app.Activity
-import android.content.ComponentName
 import android.content.ContentResolver
+import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.support.v4.media.MediaBrowserCompat
-import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.session.MediaControllerCompat
 import android.support.v4.media.session.PlaybackStateCompat
 import android.util.Log
@@ -19,8 +16,9 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.android.botify.R
 import com.example.android.botify.databinding.FragmentAudioBinding
+import com.example.android.botify.menu.main.MainActivity
 
-class AudioFragment : Fragment(), AudioListener {
+class AudioFragment() : Fragment(), AudioListener {
     private val LOG_TAG = this::class.java.simpleName
 
     private lateinit var audioAdapter: AudioAdapter
@@ -29,83 +27,28 @@ class AudioFragment : Fragment(), AudioListener {
     // binding
     private lateinit var binding: FragmentAudioBinding
 
-    private lateinit var mediaBrowser: MediaBrowserCompat
-//    private lateinit var volumeControlStream: AudioManager.AudioPlaybackCallback
+    private lateinit var mediaController: MediaControllerCompat
 
-    private val connectionCallbacks = object : MediaBrowserCompat.ConnectionCallback() {
+    override fun onAttach(context: Context) {
+        val methodName = object {}.javaClass.enclosingMethod?.name
+        Log.i(LOG_TAG, methodName!!)
 
-        override fun onConnected() {
-            val methodName = object{}.javaClass.enclosingMethod?.name
-            Log.i(LOG_TAG, methodName!!)
+        super.onAttach(context)
 
-            mediaBrowser.sessionToken.also { token ->
-                val audioController = MediaControllerCompat(
-                    context,
-                    token
-                )
-
-                MediaControllerCompat.setMediaController(context as Activity, audioController)
-            }
-
-            buildTransportProtocols()
-        }
-
-        override fun onConnectionSuspended() {
-            val methodName = object{}.javaClass.enclosingMethod?.name
-            Log.i(LOG_TAG, methodName!!)
-//            super.onConnectionSuspended()
-        }
-
-        override fun onConnectionFailed() {
-            val methodName = object{}.javaClass.enclosingMethod?.name
-            Log.i(LOG_TAG, methodName!!)
-//            super.onConnectionFailed()
-        }
+        mediaController = (activity as MainActivity).getMainMediaController()
+        Log.i(LOG_TAG, "$methodName + $mediaController")
     }
 
-    private var controllerCallbacks = object : MediaControllerCompat.Callback() {
-
-        override fun onMetadataChanged(metadata: MediaMetadataCompat?) {
-            val methodName = object{}.javaClass.enclosingMethod?.name
-            Log.i(LOG_TAG, methodName!!)
-
-            super.onMetadataChanged(metadata)
-        }
-
-        override fun onPlaybackStateChanged(state: PlaybackStateCompat?) {
-            val methodName = object{}.javaClass.enclosingMethod?.name
-            Log.i(LOG_TAG, methodName!!)
-
-            super.onPlaybackStateChanged(state)
-        }
-
-        override fun onSessionDestroyed() {
-            val methodName = object{}.javaClass.enclosingMethod?.name
-            Log.i(LOG_TAG, methodName!!)
-
-            mediaBrowser.disconnect()
-            // maybe schedule a reconnection using a new MediaBrowser instance
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
 
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_audio, container, false)
-
-        mediaBrowser = MediaBrowserCompat(
-            context,
-            context?.let { ComponentName(it, AudioService::class.java) },
-            connectionCallbacks,
-            null //optional Bundle
-        )
-
-        Log.i(LOG_TAG, "$methodName + audioBrowser")
 
         val recyclerView = binding.audioList
         audioAdapter = AudioAdapter(this, audioList, this)
@@ -117,7 +60,7 @@ class AudioFragment : Fragment(), AudioListener {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
 
         super.onViewCreated(view, savedInstanceState)
@@ -125,48 +68,34 @@ class AudioFragment : Fragment(), AudioListener {
         binding.buttonAudioToMain.setOnClickListener {
             findNavController().navigate(R.id.action_AudioFragment_to_MainFragment)
         }
-    }
-
-    private fun buildTransportProtocols() {
-        val methodName = object{}.javaClass.enclosingMethod?.name
-        Log.i(LOG_TAG, methodName!!)
-
-        val mediaController = MediaControllerCompat.getMediaController(context as Activity)
 
         binding.displayAudioText.setOnClickListener {
-                // Since this is a play/pause button, you'll need to test the current state
-                // and choose the action accordingly
-                val pbStateTest = mediaController.playbackState.state
-                Log.i(LOG_TAG, "$methodName mC pS?: " + pbStateTest.toString())
+            // Since this is a play/pause button, you'll need to test the current state
+            // and choose the action accordingly
+            val pbStateTest = mediaController.playbackState.state
+            Log.i(LOG_TAG, "$methodName mC pS?: " + pbStateTest.toString())
 
-                if (pbStateTest == PlaybackStateCompat.STATE_PLAYING) {
-                    Log.i(LOG_TAG, "$methodName + sOCL if 1: " + pbStateTest.toString())
-                    mediaController.transportControls.pause()
-                    Log.i(LOG_TAG, "$methodName + sOCL if 2: " + pbStateTest.toString())
-                } else {
-                    Log.i(LOG_TAG, "$methodName + sOCL else 1: " + pbStateTest.toString())
-                    mediaController.transportControls.play()
-                    Log.i(LOG_TAG, "$methodName + sOCL else 2: " + pbStateTest.toString())
-                }
+            if (pbStateTest == PlaybackStateCompat.STATE_PLAYING) {
+                Log.i(LOG_TAG, "$methodName + sOCL if 1: " + pbStateTest.toString())
+                mediaController.transportControls.pause()
+                Log.i(LOG_TAG, "$methodName + sOCL if 2: " + pbStateTest.toString())
+            } else {
+                Log.i(LOG_TAG, "$methodName + sOCL else 1: " + pbStateTest.toString())
+                mediaController.transportControls.play()
+                Log.i(LOG_TAG, "$methodName + sOCL else 2: " + pbStateTest.toString())
             }
-
-        // Display the initial state
-        val metadata = mediaController.metadata
-        val pbState = mediaController.playbackState
-
-        // Register a Callback to stay in sync
-        mediaController.registerCallback(controllerCallbacks)
+        }
     }
 
     override fun onAudioClickListener(position: Int) {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
-
-        val mediaController = MediaControllerCompat.getMediaController(context as Activity)
 
         val pbStateTest = mediaController.playbackState.state
 
-        val uri =  Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE).authority("com.example.android.botify").path(audioList[position].audio.toString()).build()
+        val uri = Uri.Builder().scheme(ContentResolver.SCHEME_ANDROID_RESOURCE)
+            .authority("com.example.android.botify").path(audioList[position].audio.toString())
+            .build()
 
         Log.i(LOG_TAG, "$methodName mC pS?: " + pbStateTest.toString())
         Log.i(LOG_TAG, "$methodName uri: " + uri)
@@ -189,28 +118,14 @@ class AudioFragment : Fragment(), AudioListener {
     }
 
     override fun onStart() {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
 
         super.onStart()
-
-        if (!mediaBrowser.isConnected) {
-            mediaBrowser.connect()
-        }
-    }
-
-    fun getMediaController() : MediaControllerCompat? {
-
-        if (context != null) {
-            val mediaController = MediaControllerCompat.getMediaController(context as Activity)
-            return mediaController
-        } else {
-            return null
-        }
     }
 
     override fun onResume() {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
 
         super.onResume()
@@ -218,17 +133,14 @@ class AudioFragment : Fragment(), AudioListener {
     }
 
     override fun onStop() {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
 
         super.onStop()
-
-        MediaControllerCompat.getMediaController(context as Activity)?.unregisterCallback(controllerCallbacks)
-
     }
 
     override fun onDestroy() {
-        val methodName = object{}.javaClass.enclosingMethod?.name
+        val methodName = object {}.javaClass.enclosingMethod?.name
         Log.i(LOG_TAG, methodName!!)
 
         super.onDestroy()
